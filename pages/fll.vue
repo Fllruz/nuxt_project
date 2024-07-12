dc6160f4c773005f69bb3af1d885c10e0f0be3cf
Firuz <firuz.hojiev2o2o@gmail.com>
2024-07-12 21:22:39 +0500

    начальный коммит

---
+++ pages/fll.vue
@@ -0,0 +1,3 @@
+<template>
+    fllllllll
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <div>
+    <v-app>
+      <div class="btn" style="width: 200px; margin: 20px;">
+        <v-btn color="red">Click Me</v-btn>
+      </div>
+      <div style="width: 200px; margin: 20px;">
+        <v-btn color="red">Click Me</v-btn>
+      </div>
+      <div style="width: 200px; margin: 20px;">
+        <v-btn color="red">Click Me</v-btn>
+      </div>
+      <div style="width: 200px; margin: 20px;">
+        <v-btn color="red">Click Me</v-btn>
+      </div>
+      <div style="width: 200px; margin: 20px;">
+        <v-btn color="red">Click Me</v-btn>
+      </div>
+    </v-app>
+  </div>
+</template>
+
+<script>
+  export default {
+    head() {
+      // Set Meta Tags for this Page
+      title: 'Hello'
+    }
+    // ...
+  }
+</script>
